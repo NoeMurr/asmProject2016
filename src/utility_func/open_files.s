f5dc5caa27730a07905df06dea10659d2536ef6f
Nosba <nosba95@yahoo.com>
2016-06-15 12:04:43 +0200

    aggiornati file

--- src/utility_func/open_files.s
+++ src/utility_func/open_files.s
@@ -2,7 +2,8 @@
 # output, i file descriptor vengono inseriti in variabili globali
 # si suppone che il nome dei due file siano salvati nell'indirizzi contenuti
 # rispettivamente in %eax(input) ed in %ebx(output)
-
+.code32														# per indicare all' assemblatore di assemblare
+																	# a 32 bit
 .section .data
 	input_fd: .long 0 							# variabile globale che conterra' il file
 									  							# descriptor del file di input
@@ -17,7 +18,7 @@
 	.globl 	output_fd 							#dichiaro la variabile come globale
 
 _open_files:
-	pushl 	%ebx 										#pusho l' indirizzo del file di output sullo
+	pushl		%ebx 									 	#pusho l' indirizzo del file di output sullo
 																	# stack
 
 	movl 		%eax, %ebx 							# sposto l' indirizzo del file che vado
@@ -27,16 +28,16 @@ _open_files:
 	movl 		$0, %ecx 								# read-only mode
 	int 		$0x80 									# apro il file
 
-	movl 		%eax, input_fd 					# metto il file descriptor nella sua variabile
+	movl 		%eax, input_fd					# metto il file descriptor nella sua variabile
 
-	popl 		$ebx 										# riprendo l' indirizzo del nome del file
+	popl 		%ebx 										# riprendo l' indirizzo del nome del file
 																	# di output che avevo messo sullo stack
 
 	movl 		$5, %eax 								# chiamata di sistema open
 	movl 		$0, %ecx 								# read-only mode
 	int 		$0x80 									# apro il file
 
-	movl 		%eax, output_fd 				# metto il file descriptor nella sua
+	movl 		%eax, output_fd		 			# metto il file descriptor nella sua
 																	# variabile come prima
 
 	ret 														# fine della funzione ed anche del file.
--- src/utility_func/tests/open_file_main_test.s
+++ src/utility_func/tests/open_file_main_test.s
@@ -1,1 +1,17 @@
 # file che serve per vedere se la funzione open_files funziona
+.section .data
+	input_file: .ascii "prova1.txt"
+	output_file: .ascii "prova2.txt"
+
+.section .text
+	.globl 	_start
+
+	_start:
+		movl 	input_file, %eax
+		movl 	output_file, %ebx
+
+		call 	_open_files
+
+		movl input_fd, %eax
+		movl output_fd, %ebx
+		
