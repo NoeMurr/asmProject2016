022751f7e638815dcd808da0791a79f69bec2d34
Alessandro Righi <alerighi4@gmail.com>
2016-06-15 18:02:12 +0200

    Sistemati commenti

--- src/itoa.s
+++ src/itoa.s
@@ -17,34 +17,34 @@
 # Parametri di output:  
 #   EAX - Lunghezza della stringa convertita (compresiva di \0 finale)
 itoa: 
-    movl $10, %ecx   # porto il fattore moltiplicativo in RCX
-    movl %eax, %ebx  # salvo temporaneamente il valore di RAX in RBX
-    xorl %esi, %esi  # azzero il registro RSI
+    movl $10, %ecx   # porto il fattore moltiplicativo in ECX
+    movl %eax, %ebx  # salvo temporaneamente il valore di EAX in EBX
+    xorl %esi, %esi  # azzero il registro ESI
 
 _itoa_dividi: 
-    xorl %edx, %edx  # azzero RDX per fare la divisione
-    divl %ecx        # divide RAX per RCX, salva il resto in RDX
+    xorl %edx, %edx  # azzero EDX per fare la divisione
+    divl %ecx        # divide EAX per ECX, salva il resto in EDX
     incl %esi        # incrementa il contatore
-    testl %eax, %eax # se il valore di RAX non è zero ripeti il ciclo
+    testl %eax, %eax # se il valore di EAX non è zero ripeti il ciclo
     jnz _itoa_dividi
 
     addl %esi, %edi  # somma all'indirizzo del buffer il numero di caratteri del numero
-    movl %ebx, %eax  # rimette il valore da convertire in RAX
-    movl %esi, %ebx  # salvo il valore della lunghezza della stringa in RBX
+    movl %ebx, %eax  # rimette il valore da convertire in EAX
+    movl %esi, %ebx  # salvo il valore della lunghezza della stringa in EBX
 
     movl $0, (%edi)   # aggiungo un null terminator alla fine della stringa
     decl %edi         # decremento il contatore della stringa di 1
 
 _itoa_converti:
-    xorl %edx, %edx  # azzero RDX per fare la divisione
-    divl %ecx        # divido RAX per RCX, salvo il valore del resto in RDX
-    addl $48, %edx   # sommo 48 a RDX
-    movb %dl, (%edi) # sposto il byte inferiore di RDX (DL) nella locazione di memoria puntata da RDI
+    xorl %edx, %edx  # azzero EDX per fare la divisione
+    divl %ecx        # divido EAX per ECX, salvo il valore del resto in EDX
+    addl $48, %edx   # sommo 48 a EDX
+    movb %dl, (%edi) # sposto il byte inferiore di EDX (DL) nella locazione di memoria puntata da EDI
     decl %edi        # decremento il puntatore della stringa
     decl %esi        # decremento il contatore
     testl %esi, %esi # se il contatore non è 0 continua ad eseguire il loop
     jnz _itoa_converti
 
-    movl %ebx, %eax  # porto il valore della lunghezza della stringa in RAX per ritornarlo
-    incl %eax    # incremento di 2 RAX (in modo da includere il carattere newline e \0)
+    movl %ebx, %eax  # porto il valore della lunghezza della stringa in EAX per ritornarlo
+    incl %eax    # incremento di 1 EAX (in modo da includere il \0)
     ret 
