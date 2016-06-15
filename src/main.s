b566bb851d30aa65697eb6245d45edf6d3ac9949
ale <alerighi4@gmai.com>
2016-06-15 17:23:48 +0200

    Aggiunto un aiuto sul da farsi: buon lavoro!

--- src/main.s
+++ src/main.s
@@ -23,6 +23,16 @@ _start:
     # nella funzione start che comunque non ritorna
     movl %esp, %ebp
 	
+    # cosa va implementato qua:
+    # 1) controllo e parsing degli argomenti della riga di comando
+    # 2) apertura dei file, controllo corretta apertura dei file
+    # 3) entro un un loop in cui 
+    #   a) leggo una riga dal file input, se EOF salto a 4
+    #   b) elaboro la riga in una struttura dati da definire
+    #   c) scelgo cosa fare in base allo stato precedente
+    #   d) scrivo una rida nel file di output 
+    # 4) chiudo tutti i file, esco dal programma correttamente
+
     # sys_exit(0); 
     movl $SYS_EXIT, %eax
     movl $0, %ebx
