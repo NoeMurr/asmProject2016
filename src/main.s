f6135bc344637b54174fbf6c5649a73736ff5976
ale <alerighi4@gmai.com>
2016-06-15 19:02:23 +0200

    andate avanti voi

--- src/main.s
+++ src/main.s
@@ -11,9 +11,21 @@
 input_filename: .asciz "input.txt"
 output_filename: .asciz "output.txt"
 .data
-# variabili modificabili
+#NB: per ora teniamo di default STDIN ed STDOUT, se uno non apre file
+input_fd: .long STDIN 
+output_fd: .long STDOUT
 .bss
-# variabili modificalibi inizializzate a 0
+last_state: .space 1
+counter: .space 1
+
+
+.equ INPUT_BUFF_LEN, 9
+input_buff: .space INPUT_BUFF_LEN
+
+.equ OUTPUT_BUFFER_LEN, 8
+output_buff: .space OUTPUT_BUFFER_LEN
+
+
 
 # codice del programma
 .text
@@ -33,6 +45,27 @@ _start:
     #   d) scrivo una rida nel file di output 
     # 4) chiudo tutti i file, esco dal programma correttamente
 
+_main_loop:
+    
+    # sys_read(input_fd, input_buff, INPUT_BUFF_LEN)
+    movl $SYS_READ, %eax
+    movl input_fd, %ebx
+    movl $input_buff, %ecx
+    movl $INPUT_BUFF_LEN, %edx
+    int $SYSCALL
+
+    # esco se EOF
+    testl %eax, %eax
+    jz _end
+
+    # sys_write(output_fd, output_buff, INPUT_BUFF_LEN)
+    movl $SYS_WRITE, %eax
+    movl output_fd, %ebx
+    movl $output_buff, %ecx
+    movl $OUTPUT_BUFF_LEN, %edx
+    int $SYSCALL
+
+_end:
     # sys_exit(0); 
     movl $SYS_EXIT, %eax
     movl $0, %ebx
