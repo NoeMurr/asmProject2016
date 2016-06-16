2ccda23b5cdb7271668388474823cb7c880d219b
Nosba <nosba95@yahoo.com>
2016-06-16 18:44:42 +0200

    sistemate indentazioni

--- src/utility_func/open_files.s
+++ src/utility_func/open_files.s
@@ -2,42 +2,50 @@
 # output, i file descriptor vengono inseriti in variabili globali
 # si suppone che il nome dei due file siano salvati nell'indirizzi contenuti
 # rispettivamente in %eax(input) ed in %ebx(output)
-.code32														# per indicare all' assemblatore di assemblare
-																	# a 32 bit
+.code32								# per indicare all' assemblatore di assemblare
+									# a 32 bit
+.include "../syscall.inc"
 .section .data
-	input_fd: .long 0 							# variabile globale che conterra' il file
-									  							# descriptor del file di input
+	input_fd: .long 0 				# variabile globale che conterra' il file
+									# descriptor del file di input
 
-	output_fd:  .long 0 						# variabile globale che conterra' il file
-	                   							# descriptor del file di output
+	output_fd:  .long 0 			# variabile globale che conterra' il file
+	                   				# descriptor del file di output
 .section .text
-	.globl 	_open_files 						# dichiaro la funzione globale
+	.globl 	_open_files 			# dichiaro la funzione globale
 	.type 	_open_files, @function 	# dichiaro l' etichetta come una funzione
 
-	.globl 	input_fd 								#dichiaro la variabile come globale
-	.globl 	output_fd 							#dichiaro la variabile come globale
+	.globl 	input_fd 				# dichiaro la variabile come globale
+	.globl 	output_fd 				# dichiaro la variabile come globale
 
 _open_files:
-	pushl		%ebx 									 	#pusho l' indirizzo del file di output sullo
-																	# stack
 
-	movl 		%eax, %ebx 							# sposto l' indirizzo del file che vado
-																	# ad aprire in %ebx
+	pushl	%ebp
+	movl 	%esp, %ebp
 
-	movl 		$5, %eax 								# chiamata di sistema open
-	movl 		$0, %ecx 								# read-only mode
-	int 		$0x80 									# apro il file
+	pushl	%ebx 			 		# pusho l' indirizzo del file di output sullo
+									# stack
 
-	movl 		%eax, input_fd					# metto il file descriptor nella sua variabile
+	movl 	%eax, %ebx 				# sposto l' indirizzo del file che vado
+									# ad aprire in %ebx
 
-	popl 		%ebx 										# riprendo l' indirizzo del nome del file
-																	# di output che avevo messo sullo stack
+	movl 	$SYS_OPEN, %eax 		# chiamata di sistema open
+	movl 	$0, %ecx 				# read-only mode
+	int 	$SYSCALL 				# apro il file
 
-	movl 		$5, %eax 								# chiamata di sistema open
-	movl 		$0, %ecx 								# read-only mode
-	int 		$0x80 									# apro il file
+	movl 	%eax, input_fd			# metto il file descriptor nella sua
+									# variabile
 
-	movl 		%eax, output_fd		 			# metto il file descriptor nella sua
-																	# variabile come prima
+	popl 	%ebx 					# riprendo l' indirizzo del nome del file
+									# di output che avevo messo sullo stack
 
-	ret 														# fine della funzione ed anche del file.
+	movl 	$SYS_OPEN, %eax 		# chiamata di sistema open
+	movl 	$2, %ecx 				# read and write, mode
+	int 	$SYSCALL 				# apro il file
+
+	movl 	%eax, output_fd			# metto il file descriptor nella sua
+									# variabile come prima
+
+	movl 	%ebp, %esp
+  	popl	%ebp
+	ret 							# fine della funzione ed anche del file.
--- src/utility_func/tests/open_file_main_test.s
+++ src/utility_func/tests/open_file_main_test.s
@@ -1,17 +1,19 @@
 # file che serve per vedere se la funzione open_files funziona
+.code32
 .section .data
-	input_file: .asciz "prova1.txt"
-	output_file: .asciz "prova2.txt"
 
 .section .text
 	.globl 	_start
 
 	_start:
-		movl 	input_file, %eax
-		movl 	output_file, %ebx
+		popl 	%eax
+		popl 	%eax
+		popl	%eax
+		popl	%ebx
 
 		call 	_open_files
 
 		movl input_fd, %eax
 		movl output_fd, %ebx
-		
+
+		ret
